3a630167da80c00ebe88d97ffaef9df0c130f03f
Aryan Sharma <aryan180906@gmail.com>
2026-08-11 16:57:29 +0530

    Add Candies for Nephews [A] from Codeforces

---
+++ 2145/A - Candies for Nephews/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    int t ; cin >> t;
+    while(t--){
+        // totla sons - 3
+        int n ; cin >> n ; // candles
+ 
+        if (n%3==0){
+            cout<<0<<endl ;
+            
+        }else{
+ 
+        int candle_required = 3 - (n%3) ;
+        cout<< candle_required <<endl ;
+        }
+    }
+}
