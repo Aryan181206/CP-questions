9c819317056d14281203ace9613350bd848ff645
Aryan Sharma <aryan180906@gmail.com>
2026-08-11 16:58:29 +0530

    Add Required Remainder [A] from Codeforces

---
+++ 1374/A - Required Remainder/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t ; 
+    cin >> t ;
+    while(t--){
+        int x , y , n ;
+        cin >> x >> y >> n ;
+        int a = (n-y)/x;
+        // a harama qutiotnt hai
+        cout << a*x + y << endl;
+        
+    }
+ 
+ 
+}
