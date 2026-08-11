df47e40623f5365073d478f8e4b88c9dd962df0d
Aryan Sharma <aryan180906@gmail.com>
2026-08-11 17:00:05 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+   int x ; cin >> x ;
+   int s;
+   // 1 2 3 4 5
+   if(x%5 == 0){
+    s = x/5 ;
+   }
+   else{
+    s=(x/5) +1 ;
+   }
+   cout << s <<endl;
+ 
+ 
+ 
+}
