0463d77b3ff7ae137769d076d4c564c1fed5f221
Aryan Sharma <aryan180906@gmail.com>
2026-08-11 16:58:16 +0530

    Add GCD vs LCM [A] from Codeforces

---
+++ 1665/A - GCD vs LCM/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// given n -> to find  a b c d such that a+ b+ c + d = n  and 
+// gcd(a,b) = lcm(c,d)
+ 
+int main(){
+ 
+    int t ; 
+    cin >> t ;
+    while(t--){
+    
+            int n ;
+            cin >> n;
+            int a = n -3 ;
+            cout << a << " " << 1 << " " << 1 << " " << 1  << endl;
+        }
+ 
+    }
+ 
