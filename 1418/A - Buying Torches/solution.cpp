19bd28c35066cba0719fc3484d1cda062c4d8aea
Aryan Sharma <aryan180906@gmail.com>
2026-08-18 04:58:00 +0530

    Add Buying Torches [A] from Codeforces

---
+++ 1418/A - Buying Torches/solution.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define pb push_back
+#define ff first
+#define ss second
+#define nl cout << endl
+#define all(x) (x).begin(),(x).end()
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define rloop(i,a,b) for(int i=a;i>=b;i--)
+#define test int t;cin>>t;while(t--)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int MOD=1e9+7;
+const int INF=1e18;
+const double PI=3.14159265358979323846;
+const int LIMIT=3e6;
+ 
+int32_t main(){
+    fast
+    test{
+        int x, y ,k ;
+        cin >> x >> y >> k;
+ 
+        int a = k*y + k-1;
+        int b = x-1;
+ 
+        int trade1 = (a+b-1) / b ;
+ 
+        cout << trade1 + k << endl;
+    }
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
