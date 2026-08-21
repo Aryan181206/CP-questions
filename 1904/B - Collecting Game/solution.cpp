ab83ffc3d9a287df4d5fe970e7514f86b45579a9
Aryan Sharma <aryan180906@gmail.com>
2026-08-21 19:42:26 +0530

    Add Collecting Game [B] from Codeforces

---
+++ 1904/B - Collecting Game/solution.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<pair<long long, int>> a(n);
+ 
+        for (int i = 0; i < n; i++) {
+            cin >> a[i].first;
+            a[i].second = i;
+        }
+ 
+        sort(a.begin(), a.end());
+ 
+        vector<long long> pref(n);
+ 
+        pref[0] = a[0].first;
+ 
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + a[i].first;
+        }
+ 
+        vector<int> ans(n);
+ 
+        ans[n - 1] = n - 1;
+ 
+        for (int i = n - 2; i >= 0; i--) {
+            if (pref[i] >= a[i + 1].first)
+                ans[i] = ans[i + 1];
+            else
+                ans[i] = i;
+        }
+ 
+        vector<int> result(n);
+ 
+        for (int i = 0; i < n; i++) {
+            result[a[i].second] = ans[i];
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            cout << result[i] << " ";
+        }
+ 
+        cout << endl ;
+    }
+ 
+   
+}      
