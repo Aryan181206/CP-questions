eb5965bc11d6064980d60fd38cb60a9b43dd78de
Aryan Sharma <aryan180906@gmail.com>
2026-08-22 06:37:40 +0530

    Add Deja Vu [B] from Codeforces

---
+++ 1891/B - Deja Vu/solution.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define pb push_back
+#define ff first
+#define ss second
+#define nl cout << endl
+#define all(x) (x).begin(),(x).end()
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define rloop(i,a,b) for(int i=a;i>=b;i--)
+#define test int t;cin>>t;while(t--)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int MOD=1e9+7;
+const int INF=1e18;
+const double PI=3.14159265358979323846;
+const int LIMIT=3e6;
+ 
+int32_t main(){
+    test{
+        int n , q ;
+        cin >> n >> q ;
+        vector<int> a(n) ;
+        vector<int> x(q) ;
+        for(int i = 0 ;i<n;i++) cin >> a[i] ;
+        for(int i = 0 ;i<q;i++) cin >> x[i] ;
+        
+        int prev = 31 ;
+        for(int i=0; i<q ;i++){
+            if(x[i]>=prev) continue;
+            int val = pow(2,x[i]);
+            for(int j = 0 ; j<n ;j++){
+                if(a[j]%val==0){
+                    a[j] = a[j] + pow(2,x[i]-1);
+                }
+            }
+            prev = x[i] ;
+        }
+        
+        for(auto val: a ){
+            cout << val << " ";
+        }
+        nl;
+        
+    }
+}
