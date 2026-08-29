3bfbea21b8a9b0a387910fc2a68a93eabafc9e36
Aryan Sharma <aryan180906@gmail.com>
2026-08-29 11:58:59 +0530

    Add Subsequence Addition (Easy Version) [G1] from Codeforces

---
+++ 1807/G1 - Subsequence Addition (Easy Version)/solution.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define pb push_back
+#define ff first
+#define ss second
+#define nl cout << endl
+#define all(x) (x).begin(),(x).end()
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define rloop(i,a,b) for(int i=a;i>=b;i--)
+#define test int t;cin>>t;while(t--)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int MOD=1e9+7;
+const int INF=1e18;
+const double PI=3.14159265358979323846;
+const int LIMIT=3e6;
+ 
+ 
+int32_t main(){
+    fast
+    test{
+        int n ; cin >> n ;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+        sort(all(a));
+ 
+        vector<int> presumshift;
+ 
+        presumshift.pb(1);
+        int sum = 0 ;
+        
+        for(int i=0;i<n-1;i++){
+            sum = sum +a[i];
+            presumshift.pb(sum);
+        }
+        
+        // for(auto a: presumshift){
+        //     cout << a << " ";
+        // }
+        bool ans = true;
+        for(int i = 0 ; i<n ;i++){
+            if(a[i]>presumshift[i]){
+                ans = false;
+            }
+        }
+        
+        cout << (ans ? "YES" :"NO" )<< endl ; 
+ 
+        
+    }
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
